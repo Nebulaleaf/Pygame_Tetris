517f2827610862c3e791515b530963d478a596d0
Alexandra Falk-Schmidtberger <nebulaleaf@gmail.com>
2024-06-30 12:11:04 +0200

    helddown works now

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ game = Game()
 GAME_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(GAME_UPDATE, 200)
 
+# Variables to control continuous movement
+move_left_timer = 0
+move_right_timer = 0
+move_down_timer = 0
+key_press_interval = 100  # Interval in milliseconds
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,19 +41,38 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if game.game_over == True:
+            if game.game_over:
                 game.game_over = False
                 game.reset()
-            if event.key == pygame.K_LEFT and game.game_over == False:
+            if event.key == pygame.K_LEFT and not game.game_over:
                 game.move_left()
-            if event.key == pygame.K_RIGHT and game.game_over == False:
+                move_left_timer = pygame.time.get_ticks()
+            if event.key == pygame.K_RIGHT and not game.game_over:
                 game.move_right()
-            if event.key == pygame.K_DOWN and game.game_over == False:
+                move_right_timer = pygame.time.get_ticks()
+            if event.key == pygame.K_DOWN and not game.game_over:
                 game.move_down()
-            if event.key == pygame.K_UP and game.game_over == False:
+                move_down_timer = pygame.time.get_ticks()
+            if event.key == pygame.K_UP and not game.game_over:
                 game.rotate()
-        if event.type == GAME_UPDATE and game.game_over == False:
+        if event.type == GAME_UPDATE and not game.game_over:
             game.move_down()
+
+    # Check for continuous movement
+    keys = pygame.key.get_pressed()
+    current_time = pygame.time.get_ticks()
+
+    if keys[pygame.K_LEFT] and current_time - move_left_timer > key_press_interval:
+        game.move_left()
+        move_left_timer = current_time
+
+    if keys[pygame.K_RIGHT] and current_time - move_right_timer > key_press_interval:
+        game.move_right()
+        move_right_timer = current_time
+
+    if keys[pygame.K_DOWN] and current_time - move_down_timer > key_press_interval:
+        game.move_down()
+        move_down_timer = current_time
 
     # Drawing
     score_value_surface = title_font.render(str(game.score), True, Colors.white)
@@ -58,14 +83,14 @@ while True:
     screen.blit(next_surface, (365, 155))
     screen.blit(rows_surface, (365, 330))
 
-    if game.game_over == True:
+    if game.game_over:
         screen.blit(game_over_surface, (320, 500))
 
-    pygame.draw.rect(screen, Colors.grey, score_rect, 0, 10)  # Change rectangle color to grey
+    pygame.draw.rect(screen, Colors.grey, score_rect, 0, 10)
     screen.blit(score_value_surface, score_value_surface.get_rect(centerx=score_rect.centerx,
         centery=score_rect.centery))
-    pygame.draw.rect(screen, Colors.grey, next_rect, 0, 10)  # Change rectangle color to grey
-    pygame.draw.rect(screen, Colors.grey, rows_rect, 0, 10)  # Change rectangle color to grey
+    pygame.draw.rect(screen, Colors.grey, next_rect, 0, 10)
+    pygame.draw.rect(screen, Colors.grey, rows_rect, 0, 10)
     screen.blit(rows_value_surface, rows_value_surface.get_rect(centerx=rows_rect.centerx,
         centery=rows_rect.centery))
 
